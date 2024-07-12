20e4de079f84231fd87ceefbb9e9d35d636f6aa1
galatea <157182004+gggalatea@users.noreply.github.com>
2024-07-13 06:53:52 +1200

    Add div container and append squareDivs to it

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const container = document.querySelector(".container");
+
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
+    container.appendChild(squareDiv);
 }
