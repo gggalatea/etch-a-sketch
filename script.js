9b07202b56c7259cf56c5907a37a5fa805b01b31
galatea <157182004+gggalatea@users.noreply.github.com>
2024-07-13 07:16:21 +1200

    Turn divs into a grid with flexbox

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square-div")
     container.appendChild(squareDiv);
 }
